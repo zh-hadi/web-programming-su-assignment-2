25b11129110b274d0ef0dbf49621000c496c39fe
Hadiuzzaman <zhhadi50@gmail.com>
2025-11-04 23:22:19 +0600

    feat: data fetch using fetch api call for phone products

---
+++ js/script.js
@@ -0,0 +1,54 @@
+function handelSearch() {
+    const searchText = document.getElementById('search_input_text').value;
+
+    loadData(searchText);
+}
+
+const loadData =async (search) => {
+    const api = `https://openapi.programming-hero.com/api/phones?search=${search}`;
+
+    const res = await fetch(api);
+    const data = await res.json();
+
+    renderData(data.data, search);
+}
+
+const renderData = (data, search) => {
+    const productContainer = document.getElementById('product_container');
+    productContainer.innerHTML = "<div>loading</div>";
+
+    console.log(data)
+    let content = "";
+    data.forEach(item => {
+        content += `<div class="card">
+                    <div class="card-image">
+                        <img src="${item.image}">
+                    </div>
+
+                    <h3 class="cart-title">${item.phone_name}</h3>
+                    <p class="cart-description">There are many variations of passages of available, but the majority
+                        have suffered</p>
+
+                    <div class="cart-price">
+                        <span>$</span>
+                        <span>999</span>
+                    </div>
+                    
+                    <div class="cart-btn">
+                        <button class="btn-primary pointer">Show Details</button>
+                    </div>
+                </div>`
+    });
+
+    
+    if(data.length > 0){
+        document.getElementById('not-found-message')?.remove();
+        productContainer.innerHTML = content;
+    }else {
+        productContainer.insertAdjacentHTML("beforebegin", `<div id="not-found-message" class="not-found">No Phones found for "${search}"</div>`);
+    }
+}
+
+const loadingAdd = () => {
+    
+}
